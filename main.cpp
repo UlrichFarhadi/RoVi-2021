5801d14f2511d363aec98330c56a66560951c133
UlrichFarhadi <Ulrichfarhadi@hotmail.com>
2021-11-02 21:29:38 +0100

    Reachability Analysis Done (No data analysis yet, will come tonight)
    Reachability = Done
    Data Analysis = Almost Done

--- main.cpp
+++ main.cpp
@@ -8,8 +8,11 @@ std::string workcell_path = project_path + "workcell";
 
 int main(int argc, char** argv)
 {
-    std::cout << "Loading workcell" << std::endl;
-    reachabilityAnalysis();  
+    std::cout << "Loading workcell..." << std::endl;
+    //simulateCollisionLandscape(4);
+    std::cout << reachabilityAnalysis_place(0, 0, false, true) << std::endl;
+
+ 
 
     return 0;
 }
--- roviproblems/includes/p3_2.hpp
+++ roviproblems/includes/p3_2.hpp
@@ -1,1 +1,3 @@
-void reachabilityAnalysis();
+void simulateCollisionLandscape(int N);
+int reachabilityAnalysis_place(double x_pos, double y_pos, bool side, bool replay);
+int reachabilityAnalysis_pick(double x_pos, double y_pos, bool side, bool replay);
