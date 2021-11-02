b143f1f83e139db986bdac7262f91b3fcb915fc8
UlrichFarhadi <Ulrichfarhadi@hotmail.com>
2021-11-02 23:06:08 +0100

    Reachability with data analysis complete
    3_2 complete

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ std::string workcell_path = project_path + "workcell";
 int main(int argc, char** argv)
 {
     std::cout << "Loading workcell..." << std::endl;
-    //simulateCollisionLandscape(4);
-    std::cout << reachabilityAnalysis_place(0, 0, false, true) << std::endl;
+    simulateCollisionLandscape(20);
+    //std::cout << reachabilityAnalysis_place(0, 0, false, true) << std::endl;
 
  
 
