95292b7ebc6555b92e6a5374f84898a9da3f1ef3
Thobias Aggerholm <thobiasaggerholm@hotmail.com>
2021-12-09 19:40:53 +0100

    testing gitignore

--- main.cpp
+++ main.cpp
@@ -6,14 +6,11 @@
 
 int main(int argc, char** argv)
 {
-<<<<<<< HEAD
     pick_n_place_analysis();
-=======
     simulateCollisionLandscape(20);
     //std::cout << reachabilityAnalysis_place(0, 0, false, true) << std::endl;
 
  
     //reachabilityAnalysis();  
->>>>>>> 449c6ab7d07d34bb9ac359723aa667db4615db47
 
     return 0;
