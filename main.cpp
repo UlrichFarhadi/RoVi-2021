e1ba477204e9c713c65e0ac715bf2fa74f5b22e4
Thobias Aggerholm <thobiasaggerholm@hotmail.com>
2021-12-12 13:57:06 +0100

    added test

--- main.cpp
+++ main.cpp
@@ -33,7 +33,7 @@ int main(int argc, char** argv)
     //simulateCollisionLandscape(20);
     //std::cout << reachabilityAnalysis_place(0, 0, false, true) << std::endl;
     std::cout << "main started" << std::endl;
-    //test();
+    test();
 
  
     //reachabilityAnalysis();  
