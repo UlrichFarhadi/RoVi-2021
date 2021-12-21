4cf9569fd8bba8f96932135c84386d19b23647f5
UlrichFarhadi <Ulrichfarhadi@hotmail.com>
2021-12-21 02:12:16 +0100

    Donso
    Donso

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
 # RoVi-2021
 Project in Robotics (kinematics, motion planning, grasping, etc.) and Computer Vision (point clouds, pose estimation, etc.) (Using OpenCV and RobWorkStudio
 
-build:
-/usr/bin/cmake --build /home/thobias/Documents/MSc1/RoVi-2021/build --config Debug --target all -j 10 --
-
-run:
-/home/thobias/Documents/MSc1/RoVi-2021/build/rovi2021
 
 
 BEFORE RUNNING
-Check CMakeLists.txt and ensure that the path FIND_PACKAGE(RobWork REQUIRED HINTS /home/ulrich/RobWork) is set
+Check CMakeLists.txt and ensure that the path FIND_PACKAGE(RobWork REQUIRED HINTS <PATH>) and FIND_PACKAGE(RobWorkStudio REQUIRED HINTS <PATH>) is set
 correctly for your current setup.
 
-Ensure that the main.cpp file is running the function you intend to test.
+Ensure that the main.cpp file is running the function you intend to test. That is by commenting out the functions you want to test inside the main function manually
+There are comments in the main function guiding how to remove comments to run the individual tests.
--- main.cpp
+++ main.cpp
@@ -44,9 +44,13 @@ int main(int argc, char** argv)
 
 
     //--------------------- REACHABILITY ANALYSIS ----------------------//
+    // Function below is for simulating the colision landscape writing to .csv files that can be read in the folder /experiment_data/p3_2
+    // In that file there wil also be a matlab file p3_2_Contourplot.m that plots the heatmaps from the .csv files. It is dynamic so should be able to run as is no matter
+    // the parameter beow. The parameter below (20) means you create a 21x21 grid equalling 441 base positions, this is quite a lot to test for so it can be changed lower
     //simulateCollisionLandscape(20, wc);
 
     // Function below is for testing a single base position side or top grip, for either pick or place operation, replay is saved if last parameter is true
+    // The replay can be watched in the file /experiment_data/p3_2
     //std::cout << reachabilityAnalysis_pick(0.0, 0.0, true, true, wc) << std::endl;
     //std::cout << reachabilityAnalysis_place(0.0, 0.0, true, true, wc) << std::endl;
     //------------------------------------------------------------------//
@@ -54,14 +58,21 @@ int main(int argc, char** argv)
 
     //--------------------- P4_1_M3 ----------------------//
     // Generate 20 images that are going to be used for testing
-    //std::vector<std::vector<double>> ground_truth = saveTestImages();
+    std::vector<std::vector<double>> ground_truth = saveTestImages();
+
+    // Functions below can be used for testing single noise values.
     //triangulationTest(150, 1, 0.0, "ImageTest", ground_truth, wc, state);
     //triangulationTest(0, 2, 25.0, "ImageTest", ground_truth, wc, state);
     //triangulationTest(0, 2, 50.0, "ImageTest", ground_truth, wc, state);
 
     // Run tests
 
-    // Blur Tests
+    // IMPORTANT:
+    // Before any of the tests below are run, the .csv files in \experiment_data\P4_1_M3 needs to be deleted since the functions below APPEND data to those files
+    // The Matlab file Data_Analysis_P4_1M3.m only works for the parameters given below (ex 150 iterations for gaussian blur), so the plots will not work if tests are
+    // made with any less iterations. (can of course be changed manually in the matlab file)
+
+    // Blur Tests --> Remove comments for this for loop below to run the gaussian blur tests described in the report with 150 iterations
     /*
     for (int i = 0; i <= 150; i++) // Increasing the amount of times an average kernel of 3x3 is used on the image
     {
@@ -70,7 +81,7 @@ int main(int argc, char** argv)
     }
     */
 
-    // Salt and Pepper (black and white) Tests
+    // Salt and Pepper (black and white) Tests --> Remove comments for this for loop below to run the salt and pepper noise tests described in the report
     /*
     for (int i = 0; i <= 100; i++) // Increasing the percentage of Salt and Pepper noise
     {
@@ -79,7 +90,7 @@ int main(int argc, char** argv)
     }
     */
 
-    // Blur and Salt and Pepper (black and white) Tests
+    // Blur and Salt and Pepper (black and white) Tests --> Remove comments for this for loop below to run the gaussian blur combined with salt and pepper noise test described in the report
     /*
     for (int i = 0; i <= 100; i+=2) // Increasing the percentage of Salt and Pepper noise
     {
@@ -87,6 +98,7 @@ int main(int argc, char** argv)
         triangulationTest(i, 0, i, "BlurSPTest", ground_truth, wc, state);
     }
     */
+    
 
     //----------------------------------------------------//
 
